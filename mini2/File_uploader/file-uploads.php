ec4202c8610529e51edc9088b827394afa8f41db
diego zuluaga <zuluaga.rm.diego@gmail.com>
2018-04-30 12:33:34 -0400

    Removed unwanted files

--- mini2/File_uploader/file-uploads.php
+++
@@ -1,8 +0,0 @@
-<?php
-  foreach ($_FILES['files']['name'] as $i => $name) {
-    move_uploaded_file();{
-      
-    }
-  }
-
- ?>
--- mini2/conn/connMini.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // connect to MySQL as a whole (pg. 29)
-    $conn = mysqli_connect("localhost", "root", "mysql") or die("Couldn't Connect to MySQL!");
-
-    // connect to a specific database
-    mysqli_select_db($conn, "socnet2") or die("Couldn't Connect to Database!");
-?>
